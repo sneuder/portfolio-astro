d1896c4e88ac94edfe7b68b453fb98f2a8dbd480
sneuder <sneuder8@gmail.com>
2024-06-18 17:27:44 +0000

    feat(education): education data

---
+++ src/data/education.ts
@@ -0,0 +1,20 @@
+import { type Education } from '@/models/education'
+
+const education: Education[] = [
+  {
+    institute: 'Alura',
+    logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7GNQhrlzrEWBt4hyfc5Nz5VI_FA5n4k0Sbg&s',
+    degree: 'Education Backend',
+    from: new Date('2023-03-01'),
+    to: new Date('2023-09-01'),
+  },
+  {
+    institute: 'Universidad de la Amazonia',
+    logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7GNQhrlzrEWBt4hyfc5Nz5VI_FA5n4k0Sbg&s',
+    degree: "Bachelor's Degree in English",
+    from: new Date('2018-02-01'),
+    to: new Date('2022-11-01'),
+  },
+]
+
+export default education
---
+++ src/models/education.ts
@@ -0,0 +1,7 @@
+export interface Education {
+  institute: string
+  logo: string
+  degree: string
+  from: Date
+  to: Date
+}
