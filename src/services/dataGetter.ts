a84c1798f11adfad3cb5c0956e4cdafaf31de685
sneuder <sneuder8@gmail.com>
2024-07-22 13:59:25 +0000

    fix(project): position of images

--- src/services/dataGetter.ts
+++ src/services/dataGetter.ts
@@ -2,8 +2,7 @@ import { type FormatFile, FormatImage } from '@/constants/FormatFile'
 
 const getDataFromLocalBase = (src: string, typeFile: FormatFile) => {
   return async (name: string, type = typeFile) => {
-    const imageImported = await import(`/src/${src}/${name}.${type}`)
-    return imageImported.default.src.replace('/@fs/app', '') as string
+    return `/${src}/${name}.${type}`
   }
 }
 
